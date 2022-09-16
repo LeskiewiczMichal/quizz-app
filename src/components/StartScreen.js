4df7cc4b9cb2b4ffbda8fae504712227e952586a
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-09-16 10:42:11 +0200

    Starting screen

---
+++ src/components/StartScreen.js
@@ -0,0 +1,9 @@
+export default function StartScreen() {
+    return (
+        <div className="startscreen--container">
+            <h2 className="startscreen--heading">Quizzical</h2>
+            <span className="startscreen--text">Test your knowledge</span>
+            <button className="startscreen--button">Start quiz</button>
+        </div>
+    )
+}
