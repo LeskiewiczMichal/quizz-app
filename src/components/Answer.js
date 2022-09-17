aefda8d6a583479acfa2a21f14fb18deb32208ec
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-09-17 15:37:45 +0200

    Answers changing background onclick

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -2,7 +2,7 @@ export default function Answer(props)  {
     const text = props.text.replace(/(&quot\;)/g,"\"").replace(/&amp;/g, "&").replace(/&Uuml;/g, "u").replace(/&#039;/g, "'")
     return (
         <label className="answer--container">
-            <span className="answer--checkmark">
+            <span className="answer--checkmark" style={{backgroundColor: props.isChosen ? '#D6DBF5' : 'transparent'}} onClick={props.handleClick}>
                 <input type="radio" />{text}
             </span>
         </label>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,22 +1,59 @@
-import Answer from "./Answer"
+import Answer from './Answer';
+import { useState } from 'react';
 
 export default function Question(props) {
-    // needed to remove syntax $quot in fetched questions
-    const question = props.data.question.replace(/(&quot\;)/g,"\"").replace(/&amp;/g, "&").replace(/&Uuml;/g, "u").replace(/&#039;/g, "'")
+  // needed to remove syntax $quot in fetched questions
+  const question = props.data.question
+    .replace(/(&quot\;)/g, '"')
+    .replace(/&amp;/g, '&')
+    .replace(/&Uuml;/g, 'u')
+    .replace(/&#039;/g, "'");
 
-    const incorrectAnswers = props.data.incorrect_answers;
-    const allAnswers  = [...incorrectAnswers, props.data.correct_answer]
-    const shuffledAnswers = allAnswers.sort(() => Math.random() - 0.5)
+  // turn text answers into object containing text and information whether the answer is right or not
+  const incorrectAnswers = props.data.incorrect_answers.map((answer) => {
+    return {
+      text: answer,
+      isRight: false,
+      isChosen: false,
+    };
+  });
+  const allAnswers = [
+    ...incorrectAnswers,
+    { text: props.data.correct_answer, isRight: true, isChosen: false },
+  ];
+  const [shuffledAnswers, setShuffledAnswers] = useState(allAnswers.sort(
+    () => Math.random() - 0.5
+  ));
 
-    const renderAnswers = shuffledAnswers.map((answer) => {
-        return <Answer key={answer} text={answer}/>
-    })
+  function chooseAnswer(text) {
+    setShuffledAnswers((prevAnswers) => {
+      const newAnswers = prevAnswers.map((answer) => {
+        if (answer.text === text) {
+          return { ...answer, isChosen: true };
+        } else {
+          return { ...answer, isChosen: false };
+        }
+      });
+      return newAnswers
+    });
+  }
+
+  const renderAnswers = shuffledAnswers.map((answer) => {
     return (
-        <div className="question--container">
-            <h5 className="question--header">{question}</h5>
-            <div className="question--answers-container">
-                {renderAnswers}                                                          
-            </div>
-        </div>
-    )
+      <Answer
+        key={answer.text}
+        text={answer.text}
+        isRight={answer.isRight}
+        isChosen={answer.isChosen}
+        handleClick={() => chooseAnswer(answer.text)}
+      />
+    );
+  });
+
+  return (
+    <div className="question--container">
+      <h5 className="question--header">{question}</h5>
+      <div className="question--answers-container">{renderAnswers}</div>
+    </div>
+  );
 }
